72e7f1656d44d2cd1f97d0e6acc1fb137ed4bdf6
Matt Kipps <matkips@gmail.com>
2013-07-23 15:15:29 -0700

    Initial commit.
    myrottenpotatoes app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myrottenpotatoes::Application.config.secret_key_base = '7e6102a6074dfafbce4beb78f23174117025c389d1a631dcfd8c329a67655277d74b2884e7dc0a6fda18440cf1da1b90829d7e4c8b4a9be50434dbe25fb735d3'
