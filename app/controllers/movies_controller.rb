662366cd121e195934bc97f9119f5fb4a3f4884f
Matt Kipps <matkips@gmail.com>
2013-07-23 18:44:31 -0700

    Add edit and delete actions, and direct links from index table

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,18 +5,35 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params[:id])
+    @movie = Movie.find params[:id]
   end
 
   def new
   end
 
   def create
-    @movie = Movie.create!(movie_params)
+    @movie = Movie.create! movie_params
     flash[:success] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
 
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(movie_params)
+    flash[:success] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
   private
     def movie_params
       params.require(:movie).permit(:title, :rating, :description, :release_date)
