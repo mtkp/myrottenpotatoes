8250a1d2e271f69a5a267a63e9befac985b69eca
Matt Kipps <matkips@gmail.com>
2013-07-23 17:13:55 -0700

    Add new and show views.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,4 +4,21 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    redirect_to movies_path
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:title, :rating, :description, :release_date)
+    end
+
 end
