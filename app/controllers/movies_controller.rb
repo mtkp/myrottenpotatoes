298a32fe93888c02f59a8c3ffb4fcc7707b8892d
Matt Kipps <matkips@gmail.com>
2013-07-25 18:34:09 -0700

    Write specs for movies controller search_tmdb post action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -37,8 +37,9 @@ class MoviesController < ApplicationController
   end
 
   def search_tmdb
-    flash[:warning] = "'#{params[:search_terms]}' was not found in TMDb."
-    redirect_to movies_path
+    # flash[:warning] = "'#{params[:search_terms]}' was not found in TMDb."
+    # redirect_to movies_path
+    @movies = Movie.find_in_tmdb(params[:search_terms])
   end
 
   private
