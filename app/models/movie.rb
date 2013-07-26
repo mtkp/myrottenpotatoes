9c70371f732123879dfb9ac342f986e772913c2f
Matt Kipps <matkips@gmail.com>
2013-07-25 22:15:43 -0700

    Write tests for model, and turn to green

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,24 @@
 class Movie < ActiveRecord::Base
 
+  class Movie::InvalidKeyError < StandardError; end
+
+  def self.api_key
+    ENV["TMDB_API_KEY"]
+  end
+
+  def self.find_in_tmdb(string)
+    Tmdb.api_key = self.api_key
+    begin
+      TmdbMovie.find(title: string)
+    rescue ArgumentError => tmdb_error
+      raise Movie::InvalidKeyError, tmdb_error.message
+    rescue RuntimeError => tmdb_error
+      if tmdb_error.message =~ /status code '404'/
+        raise Movie::InvalidKeyError, tmdb_error.message
+      else
+        raise RuntimeError, tmdb_error.message
+      end
+    end
+  end
+
 end
