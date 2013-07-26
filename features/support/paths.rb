fc85e9d6eb22ca30c934d4114d7e00f3cd3d7d3e
Matt Kipps <matkips@gmail.com>
2013-07-26 12:27:03 -0700

    Mock the web and get tests to pass.

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -21,6 +21,9 @@ module NavigationHelpers
 
     when /the Create New Movie page/
       '/movies/new'
+
+    when /the "Search Results" page/
+      '/movies/search_tmdb'
       
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
