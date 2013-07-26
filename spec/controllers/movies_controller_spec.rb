3c1a296a0b3ceb7270228983b7a590016acd80fa
Matt Kipps <matkips@gmail.com>
2013-07-25 17:36:31 -0700

    Add gems for autotesting (guard)

---
+++ spec/controllers/movies_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe MoviesController do
+
+  describe "searching TMDb" do
+    it "should call the model method that performs TMDb search"
+    it "should select the Search Results template for rendering"
+    it "should make the TMDb search results available to that template"
+    it "blah"
+  end
+
+end
