ccaec7d44a17c6a4928861591e9b15ff97f608c1
Matt Kipps <matkips@gmail.com>
2013-07-26 12:26:33 -0700

    Install webmock (and relocate require for test helpers)

---
+++ features/support/simplecov.rb
@@ -0,0 +1,2 @@
+# for simplecov
+require 'simplecov'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # for simplecov
 require 'simplecov'
+require 'webmock/rspec'
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
