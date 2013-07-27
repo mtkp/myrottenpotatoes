2e2c1d289e80270b24e0b757a82f856889c7318a
Matt Kipps <matkips@gmail.com>
2013-07-27 12:34:03 -0700

    Add a file to disable minitest error when running cuke thru guard

---
+++ features/support/disable_mini_test_autorun.rb
@@ -0,0 +1,21 @@
+# Why: http://groups.google.com/group/cukes/browse_thread/thread/5682d41436e235d7
+begin
+  require 'minitest/unit'
+  # Don't attempt to monkeypatch if the require succeeded but didn't
+  # define the actual module.
+  #
+  # https://github.com/cucumber/cucumber/pull/93
+  # http://youtrack.jetbrains.net/issue/TW-17414
+  if defined?(MiniTest::Unit)
+    class MiniTest::Unit
+      class << self
+        @@installed_at_exit = true
+      end
+
+      def run(*)
+        0
+      end
+    end
+  end
+rescue LoadError => ignore
+end
