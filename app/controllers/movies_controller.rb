222b0c3214f5f26d29a1c4f352fe1a12140ea9d5
Matt Kipps <matkips@gmail.com>
2013-07-27 12:33:47 -0700

    Simplify controller spec, and add tests for patch :update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  before_filter :find_movie, only: [:show, :edit] 
+  before_filter :find_movie, only: [:show, :edit, :update] 
 
   def index
     @movies = Movie.order(:title)
@@ -21,7 +21,6 @@ class MoviesController < ApplicationController
   end
 
   def update
-    @movie = Movie.find_by_id params[:id]
     @movie.update_attributes!(movie_params)
     flash[:success] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
