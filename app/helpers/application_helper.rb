f8b423b73480b22f9af2f6bd6603406f48bac1e1
Matt Kipps <matkips@gmail.com>
2013-07-27 13:34:26 -0700

    DRY out displaying the release_date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def release_date(movie)
+    if movie.release_date.respond_to? :strftime
+      movie.release_date.strftime("%B %d, %Y") 
+    else
+      Time.parse(movie.release_date).strftime("%B %d, %Y")
+    end
+  rescue ArgumentError
+    ""
+  end
+
 end
