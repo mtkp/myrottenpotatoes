672c95b478bc38b514d867ad330155fe9fb779ae
Matt Kipps <matkips@gmail.com>
2013-07-27 18:21:58 -0700

    Ensure valid for 1, 1, 1930

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -16,7 +16,7 @@ class Movie < ActiveRecord::Base
 
   def released_1930_or_later
     errors.add(:release_date, "must be 1930 or later") if
-      self.release_date < Time.parse("1 Jan 1930")
+      self.release_date <= Time.parse("31 Dec 1929")
   end
 
   def self.find_in_tmdb(string)
