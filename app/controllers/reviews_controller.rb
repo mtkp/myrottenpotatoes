e5ea2e5535c290387851267ea79f5271092d18a3
Matt Kipps <matkips@gmail.com>
2013-07-31 17:54:14 -0700

    dump reviews controller for pending redesign.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,7 @@
-include ActionView::Helpers::TextHelper
-
 class ReviewsController < ApplicationController
   before_filter :has_moviegoer_and_movie
-  before_filter :find_review, only: [:edit, :update]
 
   def new
-    redirect_to edit_movie_review_path(@movie, @review) if moviegoer_has_review
     @review = @movie.reviews.build
   end
 
@@ -18,16 +14,10 @@ class ReviewsController < ApplicationController
   end
 
   def update
-    if @review.update_attributes review_params
-      potatoes = pluralize(@review.potatoes, "potato", "potatoes")
-      flash[:success] = "Your review has been updated to #{potatoes}."
-      redirect_to @movie
-    else
-      render 'edit'
-    end
   end
 
 private
+
   def has_moviegoer_and_movie
     unless @current_user
       flash[:error] = "You must be logged in to create a review."
@@ -41,16 +31,6 @@ private
 
   def review_params
     params.require(:review).permit(:movie, :moviegoer, :potatoes)
-  end
-
-  def moviegoer_has_review
-    @review = @current_user.reviews.find_by_movie_id(@movie.id)
-  end
-
-  def find_review
-    @review = Review.find_by_id params[:id]
-    redirect_to @movie unless @review
-    redirect_to @movie unless @review.moviegoer.id == @current_user.id
   end
 
 end
