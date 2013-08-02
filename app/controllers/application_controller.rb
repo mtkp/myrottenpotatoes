5c2c152dbb3519e0e9c98b9ae595ca17bc981438
Matt Kipps <matkips@gmail.com>
2013-08-02 12:17:24 -0700

    Fix indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   before_action :set_current_user
-  protected
+
+protected
   def set_current_user
     @current_user ||= Moviegoer.find_by_id(session[:user_id])
   end
