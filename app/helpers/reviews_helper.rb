9e16aefcef157f2f1a3ed4177121277de55c9b7d
Matt Kipps <matkips@gmail.com>
2013-08-02 14:21:01 -0700

    Add testing and custom colors for movie rating average

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -2,12 +2,16 @@ module ReviewsHelper
 
   def review_class(review_average)
     case review_average
-    when 0..2
-      "text-danger"
+    when 0.0
+      "no-rating"
+    when 1...2
+      "low-rating"
     when 2...3
-      "text-warning"
-    when 3..5
-      "text-success"
+      "mid-low-rating"
+    when 3...4
+      "mid-high-rating"
+    when 4..5
+      "high-rating"
     else
       ""
     end
--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -11,4 +11,38 @@ require 'spec_helper'
 #   end
 # end
 describe ReviewsHelper do
+  describe "review_class" do
+    it "returns no-rating class if the average is zero" do
+      css_class = helper.review_class(0.0)
+      css_class.should =~ /\Ano-rating\z/
+    end
+    it "returns low-rating class if the average is between 1 and 2" do
+      [1.0, 1.9].each do |i|
+        css_class = helper.review_class(i)
+        css_class.should =~ /\Alow-rating\z/
+      end
+    end
+    it "returns mid-low-rating class if the average is between 2 and 3" do
+      [2.0, 2.9].each do |i|
+        css_class = helper.review_class(i)
+        css_class.should =~ /\Amid-low-rating\z/
+      end
+    end
+    it "returns mid-high-rating class if the average is between 3 and 4" do
+      [3.0, 3.9].each do |i|
+        css_class = helper.review_class(i)
+        css_class.should =~ /\Amid-high-rating\z/
+      end
+    end
+    it "returns high-rating class if the average is between 4 and 5" do
+      [4.0, 4.9, 5.0].each do |i|
+        css_class = helper.review_class(i)
+        css_class.should =~ /\Ahigh-rating\z/
+      end
+    end
+    it "returns an empty string otherwise" do
+      css_class = helper.review_class(5.1)
+      css_class.should be_empty
+    end
+  end
 end
