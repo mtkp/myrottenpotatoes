125d4392b37aa12e86c24388ca630465cb26f6cf
Matt Kipps <matkips@gmail.com>
2013-08-02 13:53:58 -0700

    Add the class method to support omniauth implementation suggestion

--- app/models/moviegoer.rb
+++ app/models/moviegoer.rb
@@ -2,10 +2,11 @@ class Moviegoer < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   has_many :movies, through: :reviews
 
-  def self.create_with_omniauth(auth)
-    Moviegoer.create!(
-      provider: auth[:provider],
-      uid: auth[:uid],
-      name: auth[:info][:name])
+  def self.find_or_create_from_auth_hash(auth_hash)
+    self.find_or_create_by!(
+      provider: auth_hash[:provider],
+      uid: auth_hash[:uid],
+      name: auth_hash[:info][:name]
+    )
   end
 end
