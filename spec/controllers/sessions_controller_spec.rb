7afbdccebe384a786af445e275a7d6a657345e0e
Matt Kipps <matkips@gmail.com>
2013-08-02 15:13:23 -0700

    Test the sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,4 +2,33 @@ require 'spec_helper'
 
 describe SessionsController do
 
+  describe "new action" do
+    it "should call the set_current_user filter" do
+      ApplicationController.any_instance.should_receive(:set_current_user)
+      get :new
+    end
+  end
+
+  describe "create action" do
+    before :each do
+      @moviegoer = FactoryGirl.create(:moviegoer)
+    end
+    it "should not call the set_current_user filter" do
+      Moviegoer.stub(:find_or_create_from_auth_hash).and_return(@moviegoer)
+      ApplicationController.should_not_receive(:set_current_user)
+      post :create
+    end
+  end
+
+  describe "destroy action" do
+    it "should call the set_current_user filter" do
+      ApplicationController.any_instance.should_receive(:set_current_user)
+      delete :destroy
+    end
+    it "redirect to the root path" do
+      delete :destroy
+      response.should redirect_to root_path
+    end
+  end
+
 end
