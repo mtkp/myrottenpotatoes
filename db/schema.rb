f475e4945963a2693c7308a6c91291632caa5a17
Matt Kipps <matkips@gmail.com>
2013-08-06 16:17:18 -0700

    Add tmdb_id column to movies.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130806205643) do
+ActiveRecord::Schema.define(version: 20130806224254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20130806205643) do
     t.datetime "release_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "tmdb_id"
   end
 
   add_index "movies", ["title"], name: "index_movies_on_title", using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 # Seed the RottenPotatoes DB with some movies.
 movies = [
-  { title: 'Aladdin', rating: 'G',
+  { title: 'Aladdin', rating: 'G', tmdb_id: 812,
     release_date: '25-Nov-1992', description: "Aladdin, a street urchin, accidentally meets Princess Jasmine, who is in the city undercover. They love each other, but she can only marry a prince." },
-  { title: 'When Harry Met Sally', rating: 'R',
+  { title: 'When Harry Met Sally', rating: 'R', tmdb_id: 639,
     release_date: '21-Jul-1989', description: "Harry and Sally have known each other for years, and are very good friends, but they fear sex would ruin the friendship." },
-  { title: 'The Help', rating: 'PG-13',
+  { title: 'The Help', rating: 'PG-13', tmdb_id: 50014,
     release_date: '10-Aug-2011', description: "An aspiring author during the civil rights movement of the 1960s decides to write a book detailing the African-American maids' point of view on the white families for which they work, and the hardships they go through on a daily basis." },
-  { title: 'Raiders of the Lost Ark', rating: 'PG',
+  { title: 'Raiders of the Lost Ark', rating: 'PG', tmdb_id: 85,
     release_date: '12-Jun-1981', description: "Archeologist and adventurer Indiana Jones is hired by the US government to find the Ark of the Covenant before the Nazis." },
-  { title: 'Inception', rating: 'PG-13',
+  { title: 'Inception', rating: 'PG-13', tmdb_id: 27205,
     release_date: '16-Jul-2010', description: "A skilled extractor is offered a chance to regain his old life as payment for a task considered to be impossible." },
-  { title: 'Man of Steel', rating: 'PG-13',
+  { title: 'Man of Steel', rating: 'PG-13', tmdb_id: 49521,
     release_date: '14-Jun-2013', description: "A young itinerant worker is forced to confront his secret extraterrestrial heritage when Earth is invaded by members of his race." },
-  { title: 'Pacific Rim', rating: 'PG-13',
+  { title: 'Pacific Rim', rating: 'PG-13', tmdb_id: 68726,
     release_date: '12-Jul-2013', description: "As a war between humankind and monstrous sea creatures wages on, a former pilot and a trainee are paired up to drive a seemingly obsolete special weapon in a desperate effort to save the world from the apocalypse." }
 ]
 
