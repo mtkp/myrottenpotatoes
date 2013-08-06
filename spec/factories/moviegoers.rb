76eb324ace3808fb904cf1c30b9a96253345fa17
Matt Kipps <matkips@gmail.com>
2013-08-06 14:49:32 -0700

    Make a factory for admin users

--- spec/factories/moviegoers.rb
+++ spec/factories/moviegoers.rb
@@ -1,9 +1,14 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
+# generate a moviegoer
 
 FactoryGirl.define do
   factory :moviegoer do
     sequence(:name) { |n| "Person #{n}" }
     provider "twitter"
     sequence(:uid) { |n| "000#{n}" }
+
+    # generate a moviegoer that is an administrator
+    factory :admin do
+      admin true
+    end
   end
 end
