9e5ede900da6fb18584b567ce72a9bd0e7945498
Matt Kipps <matkips@gmail.com>
2013-08-14 10:39:44 -0700

    Implement jQuery filtering on index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,30 @@
 //= require turbolinks
 //= require twitter/bootstrap
 //= require_tree .
+
+RP = {
+  setup: function() {
+    // construct new DOM elements
+    $('<label for="filter" class="explanation">' +
+      'Restrict to movies suitable for children' +
+      '</label>' +
+      '<input type="checkbox" id="filter" />'
+    ).insertBefore('#movies').change(RP.filter_adult);
+  },
+  filter_adult: function() {
+    // 'this' is element that received event from checkbox action
+    if ($(this).is(':checked')) {
+      $('#movies tbody tr').each(RP.hide_if_adult_row);
+    } else {
+      $('#movies tbody tr').show();
+    }
+  },
+  hide_if_adult_row: function() {
+    if (! /^G|PG$/i.test($(this).find('td:nth-child(2)').text())) {
+      $(this).hide();
+    }
+  }
+};
+$(RP.setup);
+
+
