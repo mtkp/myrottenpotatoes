80859b61ae4d9e664a816df5a71d4e47ca5e2f2b
Matt Kipps <matkips@gmail.com>
2013-08-14 19:36:12 -0700

    Add RP code for popover. Center mini window based on screen size.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,7 +24,12 @@ RP = {
       '</label>' +
       '<input type="checkbox" id="filter" />'
     ).insertBefore('#movies').change(RP.filter_adult);
+
+    // add invisible div to end of page
+    $('<div id="movieInfo"></div>').hide().appendTo($('body'));
+    $('#movies a').click(RP.getMovieInfo);
   },
+
   filter_adult: function() {
     // 'this' is element that received event from checkbox action
     if ($(this).is(':checked')) {
@@ -33,7 +38,33 @@ RP = {
       $('#movies tbody tr').show();
     }
   },
+
+  getMovieInfo: function() {
+    $.ajax({type: 'GET',
+            url: $(this).attr('href'),
+            timeout: 5000,
+            success: RP.showMovieInfo,
+            error: function() {alert('Error!');}
+          });
+    return(false);
+  },
+
+  showMovieInfo: function(data) {
+    // center a floater 1/2 as wide and 1/4 as tall as screen
+    var oneFourth = Math.ceil($(window).width() / 4);
+    $('#movieInfo').html(data).
+      css({'left': oneFourth, 'width': 2*oneFourth, 'top': 250}).show();
+    $('closeLink').click(RP.hideMovieInfo);
+    return(false);
+  },
+
+
+  hideMovieInfo: function() {
+    $('#movieInfo').hide();
+    return(false);
+  }
 };
+
 $(RP.setup);
 
 
