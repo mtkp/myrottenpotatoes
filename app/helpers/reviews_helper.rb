f8b2baad761d29d7fe26a8d712428c656f1ab855
Matt Kipps <matkips@gmail.com>
2013-08-16 17:29:42 -0700

    Russian doll cache the reviews table that is rendered in show movie.

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -16,4 +16,11 @@ module ReviewsHelper
       ""
     end
   end
+
+  def cache_key_for_reviews_table(movie)
+    count          = movie.reviews.count
+    max_updated_at = movie.reviews.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "#{movie.title}/all-#{count}-#{max_updated_at}"
+  end
+
 end
