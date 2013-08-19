8371515fcb144718176b0682d14aa7819d036f11
Matt Kipps <matkips@gmail.com>
2013-08-19 11:43:40 -0700

    Eager load moviegoers with reviews.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,6 @@ class Review < ActiveRecord::Base
   POTATOES = (1..5).to_a
   belongs_to :movie
   belongs_to :moviegoer
-  delegate :name, to: :moviegoer, prefix: true
 
   validates :movie_id, presence: true
   validates :moviegoer_id, presence: true
