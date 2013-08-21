faa5612871a6d3c9921dd4c6258c8775f506b8dd
Matt Kipps <matkips@gmail.com>
2013-08-21 15:58:17 -0700

    Add pagination to reviews.

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,8 @@ class Review < ActiveRecord::Base
   belongs_to :movie
   belongs_to :moviegoer
 
+  self.per_page = 10
+
   validates :movie_id, presence: true
   validates :moviegoer_id, presence: true
   validates :potatoes, inclusion: POTATOES
